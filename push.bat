1783d663c6fa2f29eebcfb718f906dbff8f752bd
Amaan Ali <killeramaan12345@gmail.com>
2022-06-24 15:17:37 +0530

    improved form.. again...final

--- push.bat
+++ push.bat
@@ -8,5 +8,5 @@ git commit -m "%COM%"
 git push origin main
 git status
 Color 0A
-@echo first line&echo Second Line&echo Third line
+@echo  &echo  &echo  
 @echo Pushed and Committed succesfully (%COM%)
