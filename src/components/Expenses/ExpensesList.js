142b144367031d5c6599a478444fdc88f60be353
Amaan Ali <killeramaan12345@gmail.com>
2022-06-24 18:11:24 +0530

    testing

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,21 +1,42 @@
 import React from 'react'
 import ExpenseItem from './ExpenseItem'
 import './ExpenseList.css'
+import './ExpenseItem.css'
+import Card from '../UI/Card'
 const ExpensesList = (props) => {
-	if (props.items.length === 0) {
-		return <h2 className='expenses-list__fallback'>No Expenses Found</h2>
-	}
+	// if (props.items.length === 0) {
+	// 	return <h2 className='expenses-list__fallback'>No Expenses Found</h2>
+	// }
+
+	let num = props?.items?.map((expense) => {
+		num += expense.amount
+	})
+	// let num = 0
+	// for (let i = 0; ++i; i < props?.items?.length) {
+	// 	num += props?.items[i]?.amount
+	// }
+
 	return (
 		<ul className='expenses-list'>
-			{props.items.map((expense) => (
+			{props?.items?.map((expense) => (
 				<ExpenseItem
-					key={expense.id}
-					title={expense.title}
-					subTitle={expense.subTitle}
-					amount={expense.amount}
-					date={expense.date}
+					key={expense?.id}
+					title={expense?.title}
+					subTitle={expense?.subTitle}
+					amount={expense?.amount}
+					date={expense?.date}
 				/>
 			))}
+			{props.items.length === 0 ? (
+				<h2 className='expenses-list__fallback'>No Expenses Found</h2>
+			) : (
+				<Card className='expense-item'>
+					<div className='expense-item__description'>
+						<h2>Total</h2>
+						<div className='expense-item__price'>₹ {num}</div>
+					</div>
+				</Card>
+			)}
 		</ul>
 	)
 }
